90ecbebb0185d03d1eda276b2b3bdcb33c269027
thesksingh <santankumarsingh57@gmail.com>
2023-11-04 18:50:41 +0530

    Added Entity Creation & DB Connection

--- EcomProductService/src/main/java/com/scaler/EcomProductService/model/Product.java
+++ EcomProductService/src/main/java/com/scaler/EcomProductService/model/Product.java
@@ -1,16 +1,28 @@
 package com.scaler.EcomProductService.model;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private int id;
+@Entity(name = "PRODUCT")
+public class Product extends BaseModel {
     private String title;
     private double price;
-    private String category;
     private String description;
     private String image;
+    @ManyToOne
+    private Category category;
 
 }
+
+
+/*
+    Product - Category : M : 1
+    1         1
+    M         1
+    M         1
+ */
